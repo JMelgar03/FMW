c3c41746ac37f5850e5d096d029fd686dc8fa913
JMelgar03 <melgarjonathan03@gmail.com>
2019-08-02 01:11:05 -0600

    landing 2

---
+++ public/js/controlador.js
@@ -0,0 +1,10 @@
+//Cambiar tamano de tarjeta
+function agrandar(a){
+   $("#"+a.id).css("width", "95%", "height","800px");
+}
+
+function encoger(a){
+    $("#"+a.id).css("width", "80%", "height","300px");
+ }
+
+ //fin cambiar tamano tarjeta
